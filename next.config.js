998c14307b3547c7cb48d4741ce910a2c35e1121
Daniel-Developement <contatodanielsilvaoficial@gmail.com>
2023-04-02 17:07:41 -0300

    fix bug env

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
   env: {
-    API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+    API_URL: 'https://track-master-three.vercel.app',
   }
 }
 
